623e038087af37f92e836654857e49ac1e45f9ad
sarfaraj-settyl <sarfaraj.a@settyl.com>
2022-09-19 17:21:15 +0530

    fixed otp authentication

--- home/authentication.py
+++ home/authentication.py
@@ -1,15 +1,37 @@
-import smtplib
+import pickle
+import base64
+import googleapiclient.discovery
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
+
+creds = pickle.load(open('gmail.pickle', 'rb'))
+service = googleapiclient.discovery.build('gmail', 'v1', credentials=creds)
 def sendotp(receiver,otp):
-    server = smtplib.SMTP_SSL("smtp.gmail.com",465)
-    email="com.theyouthblog@gmail.com"
-    password="canttellyou"
 
-    msg= "The one time password (OTP) to sign up at THE YOUTH BLOG is " +str(otp)
-    server.login(email,password)
-    server.sendmail(email,receiver,msg)
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = 'Otp for the youthblog'
+    msg['From'] = f'theyouthblog@gmail.com'
+    msg['To'] = receiver
+    msgPlain =  "The one time password (OTP) to sign up at THE YOUTH BLOG is " +str(otp)
+    msg.attach(MIMEText(msgPlain, 'plain'))
 
-    server.quit()
+    # msgHtml = '<b>This is my first email!</b>'
+    # msg.attach(MIMEText(msgHtml, 'html'))
+
+    raw = base64.urlsafe_b64encode(msg.as_bytes())
+    raw = raw.decode()
+    body = {'raw': raw}
+
+
+    message = (
+        service.users().messages().send(
+            userId="me", body=body).execute())
+    print('Message Id: %s' % message['id'])
+
+
+
+
 
 
 
